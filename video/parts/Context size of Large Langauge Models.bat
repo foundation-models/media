9f61a534cf64eef9947147029bc1915cda331aec
Hossein Akhlaghpour <hossein.akhlaghpour@intapp.com>
2023-05-20 16:05:39 -0700

    context size

---
+++ video/parts/Context size of Large Langauge Models.bat
@@ -0,0 +1,12 @@
+dvc add "neural networks are stateless.mkv"
+dvc add "context size of chatgpt.mkv"
+dvc add "context is important for llm.mkv"
+dvc add "training or fine tuning llm is very costly.mkv"
+dvc add "zero shot leanring and translation.mkv"
+
+dvc push "neural networks are stateless.mkv"
+dvc push "context size of chatgpt.mkv"
+dvc push "context is important for llm.mkv"
+dvc push "training or fine tuning llm is very costly.mkv"
+dvc push "zero shot leanring and translation.mkv"
+

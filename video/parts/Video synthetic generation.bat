4b884dfc1c215167168f6dffe5b0798f3da99fba
Hossein Akhlaghpour <hossein.akhlaghpour@intapp.com>
2023-05-20 18:17:21 -0700

    video gen

---
+++ video/parts/Video synthetic generation.bat
@@ -0,0 +1,26 @@
+dvc add "correct english with ChatGPT.mkv"
+dvc add "how long for gpt4 video correction.mkv"
+dvc add "avatar generation.mkv"
+dvc add "synthesia introduction.mkv"
+dvc add "synthesia crunchbase.mkv"
+dvc add "synthesia avators are not good enough.mkv"
+dvc add "video generated from my photo.mkv"
+dvc add "how long it takes to be open to public.mkv"
+
+dvc add "audio to generate synthetic video.m4a"
+dvc add "synthtic video generated from photo and audio.mp4"
+
+
+dvc push "correct english with ChatGPT.mkv"
+dvc push "how long for gpt4 video correction.mkv"
+dvc push "avatar generation.mkv"
+dvc push "synthesia introduction.mkv"
+dvc push "synthesia crunchbase.mkv"
+dvc push "synthesia avators are not good enough.mkv"
+dvc push "video generated from my photo.mkv"
+dvc push "how long it takes to be open to public.mkv"
+
+dvc push "audio to generate synthetic video.m4a"
+dvc push "synthtic video generated from photo and audio.mp4"
+
+

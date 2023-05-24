852766d285f08e319dfc314d5d9d0987348dbca2
Hossein AKhlaghpour <hossein.akhlaghpour@intapp.com>
2023-05-24 06:37:50 -0700

    xxx

--- video/create_video_gen_script.sh
+++ video/create_video_gen_script.sh
@@ -1,5 +1,12 @@
-# cd parts
-# ls -rt *.mkv | sed "s/.*/dvc add '&'/" > dvc_add.sh
-# ls -rt *.mkv | sed "s/.*/dvc push '&'/" >> dvc_add.sh
-# sh dvc_add.sh
-ls -rt parts/*.mkv | sed "s/.*/file '&'/" > $1.txt
+if [ ! -f $1.txt ]
+then
+    cd parts
+    ls -rt *.mkv | sed "s/.*/dvc add '&'/" > dvc_add.sh
+    ls -rt *.mkv | sed "s/.*/dvc push '&'/" >> dvc_add.sh
+    sh dvc_add.sh
+    ls -rt parts/*.mkv | sed "s/.*/file '&'/" > $1.txt
+else
+    # sh $1.pull.sh
+    # ffmpeg -f concat -safe 0 -i "$1.txt" -c copy "$1.mkv"
+    ffmpeg -i "$1.mkv" -codec copy "$1.mp4"
+fi

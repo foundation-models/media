a01f1d567db86ff8a9334b0acf72e8e2ea2babe8
Hossein Akhlaghpour <hossein.akhlaghpour@intapp.com>
2023-05-31 07:24:19 -0700

    sleep scripts

--- video/create_video_gen_script.sh
+++ video/create_video_gen_script.sh
@@ -3,8 +3,8 @@ then
     cd parts
     ls -rt *.mkv | sed "s/.*/dvc add '&'/" > dvc_add.sh
     ls -rt *.mkv | sed "s/.*/dvc push '&'/" >> dvc_add.sh
-    sh dvc_add.sh
-    ls -rt parts/*.mkv | sed "s/.*/file '&'/" > $1.txt
+    ls -rt *.mkv | sed "s/.*/file '&'/" > ../$1.txt
+    # sh dvc_add.sh
 else
     # sh $1.pull.sh
     # ffmpeg -f concat -safe 0 -i "$1.txt" -c copy "$1.mkv"

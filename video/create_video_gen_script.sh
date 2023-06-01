a314097e6416e009e7ccc09254cee914b5cf1cd6
Hossein AKhlaghpour <hossein.akhlaghpour@intapp.com>
2023-06-01 15:51:16 -0700

    xxx

--- video/create_video_gen_script.sh
+++ video/create_video_gen_script.sh
@@ -1,17 +1,17 @@
-if [ ! -f $1.txt ]
+if [ ! -f parts/$1.txt ]
 then
     cd parts
     ls -rt *.mkv | sed "s/.*/dvc add '&'/" > dvc_add.sh
     ls -rt *.mkv | sed "s/.*/dvc push '&'/" >> dvc_add.sh
     ls -rt *.mkv | sed "s/.*/dvc pull '&'/" >> $1.dvc_pull.sh
-    ls -rt *.mkv | sed "s/.*/file '&'/" > ../$1.txt
+    ls -rt *.mkv | sed "s/.*/file '&'/" > $1.txt
     sh dvc_add.sh
 else
     cd parts
     sh $1.dvc_pull.sh
-    cd ..
-    ffmpeg -f concat -safe 0 -i "$1.txt" -c copy "$1.mkv"
+    ffmpeg -f concat -safe 0 -i "$1.txt" -c copy "../$1.mkv"
     # ffmpeg -f concat -i "$1.txt" -vcodec copy -acodec copy merged.MOV
     # ffmpeg -f concat -i files_to_combine.txt -vcodec copy -acodec copy merged.MOV
+    cd ..
     ffmpeg -i "$1.mkv" -codec copy "$1.mp4"
 fi
